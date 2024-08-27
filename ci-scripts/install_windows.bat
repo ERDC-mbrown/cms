d2d7afe966949dfe914f2a9c34f1f7ad18526c1d
Gage Larsen <glarsen@aquaveo.com>
2024-08-27 14:01:22 -0600

    remove path

--- ci-scripts/install_windows.bat
+++ ci-scripts/install_windows.bat
@@ -1,5 +1,5 @@
 curl -v -o intel-mkl-installer.exe "%WINDOWS_HPCKIT_URL%"
-intel-mkl-installer.exe -s -x -f /d/a/webimage_extracted --log extract.log 
+intel-mkl-installer.exe -s -x -f webimage_extracted --log extract.log 
 rm -v intel-mkl-installer.exe
 dir
-webimage_extracted\bootstrapper.exe -s --action install --config ./build/oneapi-install-config-x64-windows.ini --log-dir=.
+webimage_extracted\bootstrapper.exe -s --action install --config .\build\oneapi-install-config-x64-windows.ini --log-dir=.
