6691581822c3ec33da9a194729289a3a392fcd5a
Gage Larsen <glarsen@aquaveo.com>
2024-08-27 14:07:47 -0600

    add the ini file

--- ci-scripts/install_windows.bat
+++ ci-scripts/install_windows.bat
@@ -2,4 +2,4 @@ curl -v -o intel-mkl-installer.exe "%WINDOWS_HPCKIT_URL%"
 intel-mkl-installer.exe -s -x -f webimage_extracted --log extract.log 
 rm -v intel-mkl-installer.exe
 dir
-webimage_extracted\bootstrapper.exe -s --action install --config .\build\oneapi-install-config-x64-windows.ini --log-dir=.
+webimage_extracted\bootstrapper.exe -s --action install --config ci-scripts\oneapi-install-config-x64-windows.ini --log-dir=.
