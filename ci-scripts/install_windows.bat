42d7ff2de0b75aa2558feabc30603b6beee7a852
Gage Larsen <glarsen@aquaveo.com>
2024-08-27 13:58:48 -0600

    fix slashes

--- ci-scripts/install_windows.bat
+++ ci-scripts/install_windows.bat
@@ -1,4 +1,5 @@
 curl -v -o intel-mkl-installer.exe "%WINDOWS_HPCKIT_URL%"
 intel-mkl-installer.exe -s -x -f /d/a/webimage_extracted --log extract.log 
 rm -v intel-mkl-installer.exe
-webimage_extracted/bootstrapper.exe -s --action install --config ./build/oneapi-install-config-x64-windows.ini --log-dir=.
+dir
+webimage_extracted\bootstrapper.exe -s --action install --config ./build/oneapi-install-config-x64-windows.ini --log-dir=.
